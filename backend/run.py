298af330c938fb02e30e7f4b1349422eb2d13472
hfu314 <fu420@purdue.edu>
2025-02-22 17:12:11 -0500

    add web scraping for boilergrade

--- backend/run.py
+++ backend/run.py
@@ -4,9 +4,6 @@ from web_scraper.routes import *
 from dotenv import load_dotenv
 import os
 
-# load .env file
-load_dotenv()
-
 # create flask app
 app = Flask(__name__)
 
@@ -14,7 +11,11 @@ app = Flask(__name__)
 app.config["GEMINI_API_KEY"] = os.getenv("GEMINI_API_KEY")
 app.config["GEMINI_MODEL"] = os.getenv("GEMINI_MODEL")
 
-
 # set up routes
 app.add_url_rule("/analyze_pdf", view_func=analyze_pdf, methods=["POST"])
 app.add_url_rule("/get_ratemyprof_info", view_func=get_ratemyprof_info, methods=["POST"])
+app.add_url_rule("/get_boilergrade_info", view_func=get_boilergrade_info, methods=["POST"])
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
