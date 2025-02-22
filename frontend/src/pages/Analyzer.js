72a79fcf4c2e6bbcc3ad2a3e2dd0b5d6c130d589
Alina Li <alina.li@outlook.com>
2025-02-22 10:37:37 -0500

    Add basic stuff to analyzer page

--- frontend/src/pages/Analyzer.js
+++ frontend/src/pages/Analyzer.js
@@ -1,10 +1,52 @@
+import Grid from '@mui/material/Grid2';
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import { styled } from '@mui/material/styles';
+import { Typography } from '@mui/material';
 
-import Grid from '@mui/material/Grid';
+const Item = styled(Paper)(({ theme }) => ({
+    backgroundColor: theme.palette.primary.main,
+    padding: theme.spacing(1),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+}));
+
+const Header = styled(Typography)(({ theme }) => ({
+    ...theme.typography.h4,
+    color: theme.palette.secondary.main,
+}));
+
+const Value = styled(Typography)(({ theme }) => ({
+    ...theme.typography.body1,
+    color: theme.palette.primary.contrastText,
+}));
 
 export const Analyzer = (props) => {
     const { data } = props;
 
     return (
-        <div><p>analyzer component</p></div>
+        <Box sx={{ flexGrow: 1 }}>
+            <Grid container spacing={2}>
+                <Grid size={8}>
+                    <Item>
+                        <Header>
+                            Title
+                        </Header>
+                        <Value>
+                            Text
+                        </Value>
+                    </Item>
+                </Grid>
+                <Grid size={4}>
+                    <Item>size=4</Item>
+                </Grid>
+                <Grid size={4}>
+                    <Item>size=4</Item>
+                </Grid>
+                <Grid size={8}>
+                    <Item>size=8</Item>
+                </Grid>
+            </Grid>
+        </Box>
     )
 }
